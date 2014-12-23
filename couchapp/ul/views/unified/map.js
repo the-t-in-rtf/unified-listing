90815dde7390ca97f65826b7229d4fa5237810a8
Tony <tony@raisingthefloor.org>
2014-12-23 18:31:08 +0100

    Opened up the map function as we are no longer testing poorly performing reduce functions.

--- couchapp/ul/views/unified/map.js
+++ couchapp/ul/views/unified/map.js
@@ -1,7 +1,5 @@
 function(doc) {
     "use strict";
-    if (doc.name.toLowerCase().indexOf("speech") !== -1) {
-        var key = doc.uid ? doc.uid : doc.source + ":" + doc.sid;
-        emit(key, doc);
-    }
+    var key = doc.uid ? doc.uid : doc.source + ":" + doc.sid;
+    emit(key, doc);
 }
